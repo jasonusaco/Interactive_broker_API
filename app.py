455f06434503b6a991f08577c2c118763b56efb2
Jason Yang <yyang56@ucsc.edu>
2021-09-13 22:02:31 +0800

    Add contract

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
 
 
 class TradingApp(EWrapper, EClient):
@@ -9,7 +10,18 @@ class TradingApp(EWrapper, EClient):
     def error(self, req_id, error_code, error_string):
         print(f"Error {req_id} {error_code} {error_string}")
 
+    def contractDetails(self, req_id, contract_details):
+        print(f"redID: {req_id}, contract:{contract_details}")
+
 
 app = TradingApp()
 app.connect("127.0.0.1", 7497, clientId=1)
+
+contract = Contract()
+contract.symbol = "AAPL"
+contract.secType = "STK"
+contract.currency = "USD"
+contract.exchange = "SMART"
+
+app.reqContractDetails(100, contract)
 app.run()
